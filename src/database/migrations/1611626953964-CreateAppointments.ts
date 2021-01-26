8ac857990d23d76082a5cd744f9ee7994776c0df
Everton <everton.aaraujo@outlook.com>
2021-01-26 09:45:56 -0300

    db connection for appointments

--- src/database/migrations/1611626953964-CreateAppointments.ts
+++ src/database/migrations/1611626953964-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1611626953964
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
